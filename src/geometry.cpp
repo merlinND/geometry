7819bebf7a9a13470feecee32a16da91cc58e2ef
Brunat Romain <rbrunat@if501-219-15.insa-lyon.fr>
2014-01-06 16:01:00 +0100

    added Test 2 through 13 , tests C, R

---
+++ src/geometry.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	cout << "Hello geometry!" << endl;
+	vector<int> myVector;
+	cout << myVector.max_size() << endl;
+	return 0;
+}
